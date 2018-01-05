e4a4eebe2c7430d63fea33be60a973f468f14af0
Maria Stezhko <stezhko@gmail.com>
2018-01-05 12:38:57 -0800

    Add functionality using third-party APIs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 // Declare global variables.
 var map;
-//var bounds;
 
+// Foursquare
+var clientID = 'IYUJHZXT1R3ATZJOWVZC5EFRXKW40XZSCUM0BYHDR25JNVOS';
+var clientSecret = 'YN5ETU0EMHHUHWQLR4QRBCVOSMKBOLNX5UJW2PV2CI1EF3PW';
+
+// Initialize Google Map.
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.3998683, lng: -122.1105936},
@@ -18,10 +22,11 @@ var Location = function(data) {
   var self = this;
   var defaultIcon = makeMarkerIcon('0091ff');
 
-  this.title = data.title;
-  //this.lat = data.lat;
-  //this.lng = data.lng;
   this.position = data.location;
+  this.lat = data.location.lat;
+  this.lng = data.location.lng;
+  this.title = data.title;
+  this.category = "";
   this.street = "";
   this.city = "";
 
@@ -29,51 +34,57 @@ var Location = function(data) {
   // Visibility is subject to change using a filter.
   this.visible = ko.observable(true);
 
-  // Create a marker for the location.
-  this.marker = new google.maps.Marker({
-    //position: data.location,
-    position: this.position,
-    title: this.title,
-    icon: defaultIcon,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    infoWindow: new google.maps.InfoWindow({
-      content: self.title + 'Marker Description'
-    }),
+  // Foursquare API request URL
+  var fourSquareURL = 'https://api.foursquare.com/v2/venues/search' +
+      '?ll=' + this.lat + ',' + this.lng +
+      '&client_id=' + clientID + '&client_secret=' + clientSecret +
+      '&query=' + this.title + '&v=20180104';
+
+  // Get Foursquare API data
+  $.getJSON(fourSquareURL).done(function(result) {
+      self.category = result.response.venues[0].categories[0].name;
+      self.street = result.response.venues[0].location.address;
+      self.city = result.response.venues[0].location.formattedAddress[1];
+      //console.log('result', result);
    });
 
-   // Create an information window for the location.
-   //this.infoWindow = new google.maps.InfoWindow({
-    // content: self.title + 'Marker Description'
-  // });
+  // Create a marker for the location.
+  this.marker = new google.maps.Marker({
+      position: this.position,
+      title: this.title,
+      icon: defaultIcon,
+      map: map,
+      animation: google.maps.Animation.DROP,
+   });
 
    // Show filtered markers (initially all the markers are shown).
    this.showMarker = ko.computed(function() {
-     if (this.visible() === true) {
-       this.marker.setMap(map);
-     } else {
-       this.marker.setMap(null);
-     }
-     return true
+      if (this.visible() === true) {
+          this.marker.setMap(map);
+      } else {
+          this.marker.setMap(null);
+      }
+      return true
    }, this);
 
    // When a marker is clicked, open the information window
    // and bounce a marker one time.
    this.marker.addListener('click', function() {
-     //self.contentString = 'Marker Description';
-     //self.infoWindow.setContent('dfhj');
-     console.log(self.infoWindow);
-     self.marker.infoWindow.open(map, this);
-     self.marker.setAnimation(google.maps.Animation.BOUNCE);
-     setTimeout(function() {
-       self.marker.setAnimation(null);
-     }, 750);
+      self.contentString = '<div class="info-window"><p class="title"><b>' + self.title + '</b></p>' +
+           '<p>' + self.category + '</p>' + '<p>' + self.street + '</p>' + '<p>' + self.city + '</p></div>';
+           //console.log(self.contentString);
+      infoWindow = new google.maps.InfoWindow({content: self.contentString});
+      infoWindow.open(map, this);
+      self.marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function() {
+          self.marker.setAnimation(null);
+      }, 750);
    });
 
-   // When a name in the list view is clicked,
+   // When a location in the list view is clicked,
    // open the information window for the associated marker.
    this.listClicked = function() {
-     google.maps.event.trigger(self.marker, 'click');
+      google.maps.event.trigger(self.marker, 'click');
    };
 
 };
@@ -81,19 +92,17 @@ var Location = function(data) {
 var viewModel = function() {
     var self = this;
 
-    this.locationList = ko.observableArray([]);
+    this.listItems = ko.observableArray([]);
+    this.searchInput = ko.observable('');
 
     // Populate the list view.
     locations.forEach(function(locationName) {
-      self.locationList.push( new Location(locationName) );
+      self.listItems.push( new Location(locationName) );
     });
 
+    
 
 }
-//function startApp() {
-  //ko.applyBindings(new viewModel());
-//}
-
 
 // This function takes in a COLOR, and then creates a new marker
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
@@ -108,34 +117,3 @@ function makeMarkerIcon(markerColor) {
         new google.maps.Size(21, 34));
     return markerImage;
 }
-
-/*function setMarkers() {
-  var defaultIcon = makeMarkerIcon('0091ff');
-  for (var i=0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      //map: map,
-      position: position,
-      title: title,
-      icon: defaultIcon,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    console.log('hi');
-    markers.push(marker);
-  }
-}
-
-// This function will loop through the markers array and display them all.
-function showMarkers() {
-  var bounds = new google.maps.LatLngBounds();
-  // // Extend the boundaries of the map for each marker and display the marker.
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-    bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
-}*/
